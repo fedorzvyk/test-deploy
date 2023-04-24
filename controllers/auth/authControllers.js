6b9812cb58032e378c11891d2f3640b2c4aaca07
fedorzvyk <fedorzvyk@gmail.com>
2023-04-24 22:36:00 +0300

    copy files

---
+++ controllers/auth/authControllers.js
@@ -0,0 +1,106 @@
+const sendEmail = require('../../service/mailService');
+
+const {
+  createUser,
+  verifyUser,
+  checkVerification,
+  logUser,
+  saveTokenForUser,
+} = require('../../utils/authUtils');
+const { signToken } = require('../../service/JWTServices');
+const AppError = require('../../utils/appError');
+
+const postUser = async (req, res, next) => {
+  const newUser = await createUser(req.body);
+
+  if (!newUser) {
+    return next(new AppError(409, 'Email in use'));
+  }
+
+  newUser.password = undefined;
+
+  const { name, email, verificationToken } = newUser;
+
+  const verifyEmail = {
+    to: email,
+    subject: 'Test email',
+    html: `<strong>Please verify your email</strong> <a target="_blank" href="${process.env.DEV_URL}/api/auth/verify/${verificationToken}"> Click the link </a>`,
+  };
+
+  await sendEmail(verifyEmail);
+
+  res.status(201).json({ user: { name, email } });
+};
+
+const getUserVerification = async (req, res) => {
+  const { verificationToken } = req.params;
+
+  const userHasVerifyToken = await verifyUser(verificationToken);
+
+  if (!userHasVerifyToken) {
+    return next(new AppError(404, 'User not found'));
+  }
+
+  res.status(200).json({ message: 'Verification is successful' });
+};
+
+const postVerifiedUser = async (req, res) => {
+  const { email } = req.body;
+
+  const verifiedUser = await checkVerification({ email });
+
+  if (!verifiedUser) {
+    return next(new AppError(404, 'User not found'));
+  }
+  if (verifiedUser.verify) {
+    return next(new AppError(400, 'Verification has already been passed'));
+  }
+
+  const verifyEmail = {
+    to: email,
+    subject: 'Test email',
+    html: `<strong>Please verify your email</strong> <a target="blank" href="${process.env.DEV_URL}/api/auth/verify/${verifiedUser.verificationToken}"> Click the link </a>`,
+  };
+
+  await sendEmail(verifyEmail);
+
+  res.status(200).json({ message: 'Verification email sent' });
+};
+
+const postLoggedUser = async (req, res) => {
+  const { password } = req.body;
+
+  const user = await logUser(req.body);
+
+  if (!user) {
+    return next(new AppError(401, 'Email or password is wrong'));
+  }
+
+  if (!user.verify) {
+    return next(new AppError(401, 'Email or password is wrong'));
+  }
+
+  const passwordIsValid = await user.checkPassword(password, user.password);
+
+  if (!passwordIsValid) {
+    return next(new AppError(401, 'Email or password is wrong'));
+  }
+
+  user.password = undefined;
+
+  const token = signToken(user.id);
+
+  const { id, name, email, verify } = await saveTokenForUser(user.id, {
+    token,
+    user,
+  });
+
+  res.status(200).json({ user: { id, name, email, verify, token } });
+};
+
+module.exports = {
+  postUser,
+  postLoggedUser,
+  getUserVerification,
+  postVerifiedUser,
+};
---
+++ models/taskModel.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const taskSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'add your task'],
+    },
+    start: {
+      type: String,
+      required: true,
+    },
+    end: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    month: {
+      type: String,
+      required: true,
+    },
+    day: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
+    priority: {
+      type: String,
+      enum: ['low', 'medium', 'high'],
+      default: 'low',
+    },
+    status: {
+      type: String,
+      enum: ['toDo', 'inProgress', 'done'],
+      default: 'toDo',
+    },
+  },
+  { versionKey: false }
+);
+
+const Task = model('task', taskSchema);
+module.exports = Task;
